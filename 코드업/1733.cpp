8434136e809faa05d96815e90cde8b520b4f4d51
Joonsoo <137869562+Joonsoooo@users.noreply.github.com>
2025-04-12 19:24:15 +0900

    Create 1733.cpp

---
+++ 코드업/1733.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string Sen;
+
+	cin >> Sen;
+
+	if (Sen == "IOI")
+	{
+		cout << "IOI is the International Olympiad in Informatics.";
+	}
+	else
+	{
+		cout << "I don't care.";
+	}
+}
