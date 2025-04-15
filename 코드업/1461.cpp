8023a39211021611a851eb2f453c84e121c03330
Joonsoo <137869562+Joonsoooo@users.noreply.github.com>
2025-04-16 00:53:50 +0900

    Create 1461.cpp

---
+++ 코드업/1461.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, num = 1;
+
+	cin >> n;
+
+	int** Array2 = new int* [n];
+
+	for (int i = 0; i < n; i++)
+	{
+		Array2[i] = new int[n];
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = n -1; j >= 0; j--)
+		{
+			Array2[i][j] = num;
+			num++;
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << Array2[i][j] << " ";
+		}
+		cout << "\n";
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		delete[] Array2[i];
+	}
+
+	delete[] Array2;
+	Array2 = nullptr;
+}
