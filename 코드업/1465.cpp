15b059e8bc337409862b90f23a7c8cb0df87f092
Joonsoo <137869562+Joonsoooo@users.noreply.github.com>
2025-04-18 14:41:37 +0900

    Create 1465.cpp

---
+++ 코드업/1465.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n, m, num = 1;
+
+	cin >> n >> m;
+
+	int** Array2 = new int* [n];
+	for (int i = 0; i < n; i++)
+	{
+		Array2[i] = new int[m];
+	}
+
+	for (int i = n-1; i >= 0; i--)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			Array2[i][j] = num;
+			num++;
+		}
+	}
+	
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cout << Array2[i][j] << " ";
+		}
+		cout << "\n";
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		delete[] Array2[i];
+	}
+
+	delete[] Array2;
+	Array2 = nullptr;
+}
