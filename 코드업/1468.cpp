971cf3aff10d5e70ffc9ae8aa4e447c01b43e1d1
Joonsoo <137869562+Joonsoooo@users.noreply.github.com>
2025-04-18 16:50:11 +0900

    Create 1468.cpp

---
+++ 코드업/1468.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n, num = 1;
+
+	cin >> n;
+
+	int** Array2 = new int* [n];
+	for (int i = 0; i < n; i++)
+	{
+		Array2[i] = new int[n];
+	}
+
+
+	for (int i = 1; i <= n; i++)
+	{
+		if (i % 2 == 0)
+		{
+			for (int j = n - 1; j >= 0; j--)
+			{
+				Array2[i-1][j] = num;
+				num++;
+			}
+		}
+		else
+		{
+			for (int j = 0; j < n; j++)
+			{
+				Array2[i - 1][j] = num;
+				num++;
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << Array2[i][j] << " ";
+		}
+		cout << "\n";
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		delete[] Array2[i];
+	}
+
+	delete[] Array2;
+	Array2 = nullptr;
+}
