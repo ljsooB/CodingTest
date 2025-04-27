49c42c7d8c31f65bc45d6bb6a10a5c39c5629fae
Joonsoo <137869562+Joonsoooo@users.noreply.github.com>
2025-04-27 10:25:22 +0900

    Create 1485.cpp

---
+++ 코드업/1485.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n, m;
+
+	cin >> n >> m;
+
+	int** Array2 = new int* [n];
+	for (int i = 0; i < n; i++)
+	{
+		Array2[i] = new int[m];
+	}
+
+	int left = 0, right = m - 1;
+	int top = 0, bottom = n - 1;
+	int num = n * m;
+
+	while (left <= right && top <= bottom)
+	{
+		for (int i = left; i <= right; i++)
+		{
+			Array2[top][i] = num--;
+		}
+		top++;
+
+		for (int i = top; i <= bottom; i++)
+		{
+			Array2[i][right] = num--;
+		}
+		right--;
+
+		if (top <= bottom)
+		{
+			for (int i = right; i >= left; i--)
+			{
+				Array2[bottom][i] = num--;
+			}
+			bottom--;
+		}
+
+		if (left <= right)
+		{
+			for (int i = bottom; i >= top; i--)
+			{
+				Array2[i][left] = num--;
+			}
+			left++;
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cout << Array2[i][j] << " ";
+		}
+		cout << "\n";
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		delete[] Array2[i];
+	}
+	delete[] Array2;
+	Array2 = nullptr;
+}
