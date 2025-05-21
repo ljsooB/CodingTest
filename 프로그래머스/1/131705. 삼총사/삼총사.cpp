2045a7a4110f4cb51dab93bddd3bd99464359c1c
Joonsoo <137869562+Joonsoooo@users.noreply.github.com>
2025-05-22 00:36:23 +0900

    [level 1] Title: 삼총사, Time: 0.02 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> number) {
+    int answer = 0;
+    
+    sort(number.begin(), number.end());
+    
+    for (int i = 0; i < number.size(); i++)
+	{
+		for (int j = i+1;  j < number.size();  j++)
+		{
+			for (int k = j+1; k < number.size(); k++)
+			{
+				if (number[i] + number[j] + number[k] == 0)
+					answer++;
+			}
+		}
+
+	}
+    
+    return answer;
+}
