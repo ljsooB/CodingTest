5adfaefec254f278b412ede810307a3a53d8d19d
Joonsoo <137869562+Joonsoooo@users.noreply.github.com>
2025-07-18 10:44:44 +0900

    [level 2] Title: 짝지어 제거하기, Time: 5.92 ms, Memory: 6.98 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int solution(string s)
+{
+    stack<int> st;
+    
+    for (int i = 0; i < s.length(); i++)
+    {
+        if(!st.empty() && st.top() == s[i])
+        {
+            st.pop();
+        }
+        else
+        {
+            st.push(s[i]);
+        }
+    }
+    
+    return st.empty() ? 1 : 0;
+}
